0b780b4b4f1bd0371b43461df35237eabf80eb72
sasquach45932 <sasquach@citromail.hu>
2024-10-08 21:23:04 +0200

    Shadow Of The Demon Lord special durations support

--- src/ts/handlebar-helpers.ts
+++ src/ts/handlebar-helpers.ts
@@ -32,27 +32,69 @@ class HandlebarHelpers {
         });
     }
 
+    #handleDemonLordRemainingTime(effect: any, remainingSeconds: any) {
+        let tokenName;
+        const specialDuration = foundry.utils.getProperty(
+            effect,
+            "flags.specialDuration",
+        );
+        if (specialDuration !== "None" && specialDuration !== undefined) {
+            tokenName = fromUuidSync(
+                effect.origin.substr(0, effect.origin.search(".Actor.")),
+            )?.name;
+            switch (specialDuration) {
+                case "TurnEndSource":
+                    remainingSeconds =
+                        game.i18n.localize("EffectsPanel.TurnEnd") +
+                        ` [${tokenName}]`;
+                    break;
+                case "TurnStartSource":
+                    remainingSeconds =
+                        game.i18n.localize("EffectsPanel.TurnStart") +
+                        ` [${tokenName}]`;
+                    break;
+                case "TurnEnd":
+                    remainingSeconds = game.i18n.localize(
+                        "EffectsPanel.TurnEnd",
+                    );
+                    break;
+                case "TurnStart":
+                    remainingSeconds = game.i18n.localize(
+                        "EffectsPanel.TurnStart",
+                    );
+                    break;
+                case "NextD20Roll":
+                    remainingSeconds = game.i18n.localize(
+                        "EffectsPanel.NextD20Roll",
+                    );
+                    break;
+                case "NextDamageRoll":
+                    remainingSeconds = game.i18n.localize(
+                        "EffectsPanel.NextDamageRoll",
+                    );
+                    break;
+                case "RestComplete":
+                    remainingSeconds = game.i18n.localize(
+                        "EffectsPanel.RestComplete",
+                    );
+                    break;
+                default:
+                    remainingSeconds = specialDuration;
+            }
+            return remainingSeconds;
+        }
+        return null;
+    }
+
     #registerRemainingTimeLabelHelper() {
         Handlebars.registerHelper("remainingTimeLabel", (effect, _options) => {
-            let remainingSeconds = effect.remainingSeconds;
+            const remainingSeconds = effect.remainingSeconds;
             if (game.system.id === "demonlord") {
-                let tokenName;
-                let specialDuration = foundry.utils.getProperty(effect, 'flags.specialDuration');
-                if (specialDuration !== 'None' && specialDuration !== undefined)
-                {
-                    tokenName = fromUuidSync(effect.origin.substr(0, effect.origin.search('.Actor.')))?.name;
-                    switch (specialDuration) {
-                      case 'TurnEndSource':
-                        remainingSeconds = `TurnEnd [${tokenName}]`;
-                        break
-                      case 'TurnStartSource':
-                        remainingSeconds = `TurnStart [${tokenName}]`;
-                        break
-                      default:
-                        remainingSeconds = specialDuration;
-                    } 
-                    return remainingSeconds; 
-                }
+                const dlResult = this.#handleDemonLordRemainingTime(
+                    effect,
+                    remainingSeconds,
+                );
+                if (dlResult) return dlResult;
             }
             if (remainingSeconds === Infinity && effect.turns) {
                 if (effect.turns === 1) {
