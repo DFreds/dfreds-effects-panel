4a3e1d7c6f62247b68ef422f5a3c28d6b6bdd73c
Derek Fredrickson <dfreds08@gmail.com>
2025-08-14 13:30:55 -0400

    Add relativeTo option to text editor

--- src/ts/app/effects-panel-app-v2.ts
+++ src/ts/app/effects-panel-app-v2.ts
@@ -84,9 +84,7 @@ class EffectsPanelAppV2 extends HandlebarsApplicationMixin(ApplicationV2) {
         const effects = this.#actorEffects;
 
         for (const effect of effects) {
-            effect.description = await TextEditor.enrichHTML(
-                effect.description,
-            );
+            effect.description = await TextEditor.enrichHTML(effect.description, { relativeTo: effect });
             if (effect.disabled && this.#settings.showDisabledEffects) {
                 if (effect.isTemporary) {
                     disabledTemporaryEffects.push(effect);
